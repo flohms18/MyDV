29a6653e37ac66dd106e285fd24b46607f53e144
flohms18 <trouilletflorent@outlook.com>
2026-03-13 10:53:23 +0100

    dvapp as index

--- MyDVProject/MyDVProject/urls.py
+++ MyDVProject/MyDVProject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('dvapp/', include("dvapp.urls")),
+    path('', include("dvapp.urls")),
     path('admin/', admin.site.urls),
 ]
