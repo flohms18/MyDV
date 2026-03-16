b7c20a2b301451577b697f6fb858f5078574457d
flohms18 <trouilletflorent@outlook.com>
2026-03-16 11:48:02 +0100

    Created Models

--- MyDVProject/dvapp/models.py
+++ MyDVProject/dvapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class GlossaryTerm(models.Model):
+    term = models.CharField(max_length=100,unique=True)
+    definition = models.TextField()
+
+    def __str__(self):
+        return self.term
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    
