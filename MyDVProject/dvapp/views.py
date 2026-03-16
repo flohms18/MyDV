2c94bf152f4e1d2c3df676bdf187fcd5bb424905
flohms18 <trouilletflorent@outlook.com>
2026-03-16 15:33:39 +0100

    New Updates

--- MyDVProject/dvapp/views.py
+++ MyDVProject/dvapp/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from django.core.paginator import Paginator
 
-from . models import Article,Category
+from . models import Article,Category, GlossaryTerm
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    obj = Article.objects.all().order_by('-published_at')    
+    paginator = Paginator(obj, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, "index.html", {
+        'obj': obj,
+        'page_obj' : page_obj,
+    })
+
+def about(request):
+    return render(request,'about.html')
+
 
 def glossary(request):
-    return render(request, 'glossary.html')
+    glossary_dict = {}
+    terms = GlossaryTerm.objects.all().order_by("term")
+    for term in terms:
+        FirstLetter = term.term[0].upper()
+        if FirstLetter not in glossary_dict:
+            glossary_dict[FirstLetter] = []
+        glossary_dict[FirstLetter].append(term)
+
+    return render(request, 'dataverse/glossary.html', {
+        'glossary_dict': glossary_dict})
 
 def about(request):
     return render(request, 'about.html')
@@ -22,3 +43,9 @@ def category_articles(request,category_id):
         'category' : category,
         'articles' : articles
     })
+
+def article_detail(request, slug):
+    article = get_object_or_404(Article, slug=slug)    
+    return render(request, "/article_detail.html", {
+        'article': article
+})
