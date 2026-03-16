e9635ecb73acc9f72262d0a49b118a6380e48b8e
flohms18 <trouilletflorent@outlook.com>
2026-03-16 14:14:28 +0100

    URLS edits

--- MyDVProject/dvapp/urls.py
+++ MyDVProject/dvapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("",views.index, name="index"),
     path("glossary",views.glossary, name="glossary"),
     path("about", views.about, name="about")
+    path("<int:category_id/",views.category.posts, name='category_posts')
 ]
