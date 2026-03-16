56976580b672fe007abef08e44852cc73253cc11
flohms18 <trouilletflorent@outlook.com>
2026-03-16 12:40:48 +0100

    Updates Admin

--- MyDVProject/dvapp/admin.py
+++ MyDVProject/dvapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import GlossaryTerm, Article, Category
 
 # Register your models here.
+
+admin.site.register(GlossaryTerm)
+admin.site.register(Article)
+admin.site.register(Category)
